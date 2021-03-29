afc367d071a8b135cb022f57c1eb6f619cb4ea17
repirisyan98 <repirisyan@icloud.com>
2021-03-29 13:49:30 +0700

    Pertemuan ke-7

--- config/adminlte.php
+++ config/adminlte.php
@@ -344,7 +344,7 @@ return [
 
     'plugins' => [
         'Datatables' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
